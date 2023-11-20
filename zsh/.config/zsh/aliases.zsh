3acdb53d660aa9c628aba70807c4894ce420a9a6
Enrique Mendoza <luis.mendoza@infotec.mx>
2023-11-20 10:23:46 -0600

    update: replace oh my zsh for zap

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,30 @@
+# Custom lists (exa is unmaintained, use eza instead)
+# alias ls="exa --icons --color=always --group-directories-first"
+# alias lst="exa --icons --color=always --tree --level=2 --long"
+# alias ll="exa -alF --icons --color=always --group-directories-first"
+# alias la="exa -a --icons --color=always --group-directories-first"
+# alias l="exa -F --icons --color=always --group-directories-first"
+# alias l.="exa -a | egrep '^\.'"
+
+# Confirm before overwriting something
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+
+# Colorize grep output (good for log files)
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias grep="grep --color=auto"
+
+# Easier to read disk
+alias df="df -h" # human-readable sizes
+alias free="free -m" # show sizes in MB
+
+# Git
+alias g="git"
+
+# Kitty
+alias icat="kitty +kitten icat"
+
+# Lazygit
+alias lg="lazygit"
---
+++ zsh/.config/zsh/exports.zsh
@@ -0,0 +1,6 @@
+# FNM
+eval "$(fnm env --use-on-cd)"
+
+# THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
