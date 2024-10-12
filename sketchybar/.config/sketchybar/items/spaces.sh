36abb000cc8958c4da92c949b48f741bd276a015
Enrique Mendoza <luis.mendoza@infotec.mx>
2024-10-12 10:07:11 -0600

    update(sketchybar): spaces padding

--- sketchybar/.config/sketchybar/items/spaces.sh
+++ sketchybar/.config/sketchybar/items/spaces.sh
@@ -9,6 +9,8 @@ do
                               icon=$sid                                     \
                               label.font="sketchybar-app-font:Regular:13.0" \
                               label.padding_right=20                        \
+                              padding_left=5                                \
+                              padding_right=5                               \
                               label.y_offset=-1                             \
                               script="$PLUGIN_DIR/space.sh"              
 done
@@ -18,6 +20,8 @@ sketchybar --add item space_separator left                             \
                                  icon.color=$ACCENT_COLOR              \
                                  icon.padding_left=4                   \
                                  label.drawing=off                     \
+                                 padding_left=5                       \
+                                 padding_right=5                       \
                                  background.drawing=off                \
                                  script="$PLUGIN_DIR/space_windows.sh" \
            --subscribe space_separator space_windows_change                           
